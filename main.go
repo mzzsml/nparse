2e9351273aec840a745bf607802e4f72a1f6001d
mzzsml <mzzsml@fastmail.com>
2025-05-29 12:32:48 +0200

    add: functionality to use command line flags.
    Add the ability for the user to specify command line flags, to change
    the behaviour of the program.

--- main.go
+++ main.go
@@ -42,18 +42,24 @@ func jsonEncode(n Nmaprun) []byte {
 }
 
 func main() {
+    // Define the `-jsonl` flag, and set it to false (disabled) by default.
+    jsonFlag := flag.Bool("jsonl", false, "output to jsonl")
     // In orded to be able to access the file passed as an argument,
     // we need to parse all the arguments.
     flag.Parse()
-    // We can access the filename with Arg(0), since it always points to the first argument
+    // We can access the filename using Arg(0), since it always points to the first argument
     // that IS NOT a flag.
     filename := flag.Arg(0)
+
+    // Decode the XML into th structs defined in types.go.
     //asd := unmarshalXml(filename)
     n := decodeXml(filename)
 
-    b := jsonEncode(n)
-    fmt.Fprintf(os.Stdout, "%s\n", b)
-
-    //jsonl
-    fmt.Fprintf(os.Stdout, "JSONL: %s\n", jsonl(n))
+    // If the flag is `-jsonl`, then use the jsonl output.
+    // Else, use the regular JSON output.
+    if *jsonFlag {
+        fmt.Fprintf(os.Stdout, "%s\n", jsonl(n))
+    } else {
+        fmt.Fprintf(os.Stdout, "%s\n", jsonEncode(n))
+    }
 }
