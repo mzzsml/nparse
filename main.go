34569dc8540b30f5ae9d7f3c791f5c8a731fdfca
mzzsml <mzzsml@fastmail.com>
2025-06-03 14:24:44 +0200

    modify: modify main.go to use new jsonl `encode` method

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ import (
 //    }
 //}
 
-// decodeXml decodes the Nmap XML into a Nmaprun struct.
-func decodeXml(f string) Nmaprun {
+// xmlDecode decodes the Nmap XML into a Nmaprun struct.
+func xmlDecode(f string) Nmaprun {
     var n Nmaprun
     filecontent, err := os.Open(f)
     if err != nil {
@@ -41,9 +41,49 @@ func jsonEncode(n Nmaprun) []byte {
     return b
 }
 
+// output prints Nparse output.
+// It prints either to stdout or to a file, depending if the relative `-o` flag is set.
+func output(b []byte, t string) {
+    if t == "-" {
+        fmt.Fprintf(os.Stdout, "%s\n", b)
+    } else {
+        file, err := os.Create(t)
+        if err != nil {
+            log.Fatal(err)
+        }
+        // Quando la funzione output() finisce, chiudiamo il file.
+        defer file.Close()
+        // FIX: il metodo Write vuole []byte, con jsonl noi abbiamo [][]byte
+        _, err = file.Write(b)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+}
+
+func outputJsonl(b [][]byte, t string) {
+    if t == "-" {
+        fmt.Fprintf(os.Stdout, "%s\n", b)
+    } else {
+        file, err := os.Create(t)
+        if err != nil {
+            log.Fatal(err)
+        }
+        // Quando la funzione output() finisce, chiudiamo il file.
+        defer file.Close()
+        // FIX: il metodo Write vuole []byte, con jsonl noi abbiamo [][]byte
+        _, err = file.Write(b[0])
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+}
+
 func main() {
     // Define the `-jsonl` flag, and set it to false (disabled) by default.
     jsonFlag := flag.Bool("jsonl", false, "output to jsonl")
+    // Define the output flag, outputs to os.Stdout by default.
+    outputFlag := flag.String("o", "-", "file to output. default = Stdout")
     // In orded to be able to access the file passed as an argument,
     // we need to parse all the arguments.
     flag.Parse()
@@ -53,13 +93,16 @@ func main() {
 
     // Decode the XML into th structs defined in types.go.
     //asd := unmarshalXml(filename)
-    n := decodeXml(filename)
+    n := xmlDecode(filename)
 
     // If the flag is `-jsonl`, then use the jsonl output.
     // Else, use the regular JSON output.
     if *jsonFlag {
-        fmt.Fprintf(os.Stdout, "%s\n", jsonl(n))
+        // Because jsonl returns a [][]byte type, and output() wants a []byte type, i had to make a separate function.
+        var j Jsonl
+        //outputJsonl(jsonl(n), *outputFlag) //old: uses old jsonl function
+        outputJsonl(j.encode(n), *outputFlag)
     } else {
-        fmt.Fprintf(os.Stdout, "%s\n", jsonEncode(n))
+        output(jsonEncode(n), *outputFlag)
     }
 }
