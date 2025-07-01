a5f945040fcdcd0af4e69fef087b7f9e6e5cbab0
mzzsml <mzzsml@fastmail.com>
2025-07-01 17:18:34 +0200

    refactor: move program files to lib directory.

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-    //"encoding/xml"
-    "encoding/json"
-    "fmt"
-    "log"
-    "os"
+	//"encoding/xml"
+	//"encoding/json"
+	//"fmt"
+	//"log"
+	//"os"
+
+	"github.com/mzzsml/nparse/lib"
 )
 
 //func unmarshalXml (f string) {
-//    var n Nmaprun
+//    var n lib.Nmaprun
 //    filecontent, err := os.Open(f)
 //    if err != nil {
 //       log.Fatal(err)
@@ -20,46 +22,6 @@ import (
 //    }
 //}
 
-// xmlDecode decodes the Nmap XML into a Nmaprun struct.
-// It takes a filename and returns a Nmaprun struct.
-func xmlDecode(f string) Nmaprun {
-    var n Nmaprun
-    filecontent, err := os.Open(f)
-    if err != nil {
-        log.Fatal(err)
-    }
-    n = parse(filecontent)
-    return n
-}
-
-// jsonEncode encodes an Nmaprun variable to JSON.
-func jsonEncode(n Nmaprun) []byte {
-    b, err := json.Marshal(&n)
-    if err != nil {
-        log.Fatal(err)
-    }
-    return b
-}
-
-// output prints Nparse output.
-// It prints either to stdout or to a file, depending if the relative `-o` flag is set.
-func output(b []byte, t string) {
-    if t == "-" {
-        fmt.Fprintf(os.Stdout, "%s\n", b)
-    } else {
-        file, err := os.Create(t)
-        if err != nil {
-            log.Fatal(err)
-        }
-        // Quando la funzione output() finisce, chiudiamo il file.
-        defer file.Close()
-        _, err = file.Write(b)
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
-}
-
 func main() {
-    parseFlags()
+	lib.ParseFlags()
 }
