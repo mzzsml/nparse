f94d395aec6a0f7eaefcb0dd482683a1b2b7ac75
mzzsml <mzzsml@fastmail.com>
2026-03-17 20:44:58 +0100

    add: possibility to the NmapStruct to hold more data; helper functions.
    - Allow the NmapStruct to hold more data. Specifically details on the
    "extraports" or the closed/filtered ports.
    - Properly parse the <hostnames> element in the proper struct field.
    - Add helper functions, in order to be able to retrieve data more
      conveniently.

--- nmapscan.go
+++ nmapscan.go
@@ -27,23 +27,65 @@ func (n *NmapScan) Json() []byte {
     return b
 }
 
-// GetHosts simply returns a list of all the Hosts contained in a NmapScan.
+// Hosts simply returns a list of all the Hosts contained in a NmapScan struct.
 func (n *NmapScan) GetHosts() []Host {
-    var hosts []Host
-    for _, h := range n.Hosts {
-        hosts = append(hosts, h)
+    return n.Hosts
+}
+
+// GetHost simply returns the information about the provided host.
+// GetHost only accepts the host's IPv4 as parameter.
+func (n *NmapScan) GetHost(addr string) Host {
+    for _, host := range n.Hosts {
+        for _, a := range host.Addrs {
+            if a.AddrType == "ipv4" && a.Addr == addr {
+                return host
+            }
+        }
     }
-    return hosts
+    return Host{}
 }
 
-// Host is a marshaled version of the hosts nodes in the XML.
+// Host holds the host node from the XML.
 type Host struct {
-    Addrs     []Address `xml:"address" json:"addrs"`
-    Hostnames []string  `xml:"host>hostnames" json:"hostnames"`
-    Ports     []Port    `xml:"ports>port" json:"ports"`
+    Status     State      `xml:"status" json:"status"`
+    Addrs      []Address  `xml:"address" json:"addrs"`
+    Hostnames  []Hostname `xml:"hostnames>hostname" json:"hostnames"`
+    Ports      []Port     `xml:"ports>port" json:"ports"`
+    ExtraPorts ExtraPort  `xml:"ports>extraports" json:"extraports"`
+    ScannedAt  string     `xml:"endtime,attr" json:"scannedAt"`
 }
 
-// Address is a marshaled version of the hosts nodes in the XML.
+// AddrData returns all the information held by Address struct associated with the
+// given address type ("ipv4" or "mac").
+func (h Host) AddrData(addrType string) Address {
+    switch addrType {
+    case "ipv4":
+        for _, addr := range h.Addrs {
+            if addr.AddrType == "ipv4" {
+                return addr
+            }
+        }
+    case "mac":
+        for _, addr := range h.Addrs {
+            if addr.AddrType == "mac" {
+                return addr
+            }
+        }
+    }
+    return Address{}
+}
+
+type Status struct {
+    State  string `xml:"state,attr" json:"state"`
+    Reason string `xml:"reason,attr" json:"reason"`
+}
+
+type Hostname struct {
+    Hostaname string `xml:"name,attr" json:"hostname"`
+    Type      string `xml:"type,attr" json:"type"`
+}
+
+// Address holds the address subnodes of the host nodes in the XML.
 type Address struct {
     Addr     string `xml:"addr,attr" json:"addr"`
     AddrType string `xml:"addrtype,attr" json:"addrType"`
@@ -52,10 +94,10 @@ type Address struct {
 
 // Port is a marshaled version of the hosts nodes in the XML.
 type Port struct {
-    Protocol string `xml:"protocol,attr" json:"protocol"`
-    PortId   int    `xml:"portid,attr" json:"portId"`
-    State    `xml:"state" json:"state"`
-    Service  `xml:"service" json:"service"`
+    Protocol string  `xml:"protocol,attr" json:"protocol"`
+    PortId   int     `xml:"portid,attr" json:"portId"`
+    State    State   `xml:"state" json:"state"`
+    Service  Service `xml:"service" json:"service"`
 }
 
 // State is a marshaled version of the hosts nodes in the XML.
@@ -70,4 +112,9 @@ type Service struct {
     Product   string `xml:"product,attr" json:"product"`
     Version   string `xml:"version,attr" json:"version"`
     Extrainfo string `xml:"extrainfo,attr" json:"extrainfo"`
+}
+
+type ExtraPort struct {
+    State string `xml:"state,attr" json:"state"`
+    Count string `xml:"count,attr" json:"count"`
 }
